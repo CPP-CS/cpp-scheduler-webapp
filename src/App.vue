3649863a5b9e90e3f7de3024c05599e7bbef1a60
Luciano Lim <chunyumikey@gmail.com>
2021-10-07 23:49:41 -0700

    created scheduling algorithm

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,69 @@
   <div id="container">
     <h1 id="title">CPP Scheduler</h1>
     <Courses @find-schedules="findSchedules" />
-    <Schedule />
+    <Schedules :schedules="schedules" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { SectionsData } from "./Classes";
+import { SectionsData, Schedule } from "./Classes";
 import Courses from "./components/Courses.vue";
-import Schedule from "./components/Schedule.vue";
+import Schedules from "./components/Schedules.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     Courses,
-    Schedule,
+    Schedules,
   },
   data() {
     return {
-      schedules: Schedule[]
-    }
+      schedules: [] as Schedule[],
+    };
   },
   methods: {
-    findSchedules(sectionsData: SectionsData[]):void {
-
+    findSchedules(courses: SectionsData[]): void {
+      console.log(courses);
+      let result: Schedule[] = [];
+      for (let section of courses[0].sections) {
+        result.push([section]);
+      }
+      for (let i = 1; i < courses.length; i++) {
+        let tempSchedules: Schedule[] = [];
+        for (let section of courses[i].sections) {
+          for (let schedule of result) {
+            let newSchedule: Schedule = [...schedule, section];
+            this.sortSchedule(newSchedule);
+            if (this.isValidSchedule(newSchedule)) tempSchedules.push(newSchedule);
+          }
+        }
+        result = tempSchedules;
+      }
+      console.log(result);
+      this.schedules = result;
+    },
+    isValidSchedule(schedule: Schedule) {
+      for (let i = 0; i < schedule.length - 1; i++) {
+        let first = schedule[i].EndTime;
+        let second = schedule[i + 1].StartTime;
+        if (first == "TBA" || second == "TBA") continue;
+        if (this.getHours(first) == this.getHours(second)) {
+          if (this.getMinutes(first) > this.getMinutes(second)) return false;
+        }
+        if (this.getHours(first) > this.getHours(second)) return false;
+      }
+      return true;
+    },
+    sortSchedule(schedule: Schedule) {
+      schedule.sort((first, second) => {
+        let one = first.StartTime;
+        let two = second.StartTime;
+        if (one == "TBA") return 1;
+        if (two == "TBA") return -1;
+        if (this.getHours(one) == this.getHours(two)) return this.getMinutes(one) - this.getMinutes(two);
+        return this.getHours(one) - this.getHours(two);
+      });
     },
     getHours(str: string): number {
       return parseInt(str.substring(0, 2));
@@ -37,4 +76,12 @@ export default defineComponent({
 });
 </script>
 
-<style></style>
+<style scoped>
+#title {
+  font-size: 5em;
+  text-align: center;
+}
+* {
+  font-family: "Noto Sans Display", sans;
+}
+</style>
--- src/components/Schedules.vue
+++ src/components/Schedules.vue
@@ -2,8 +2,8 @@
   <div id="schedulesContainer">
     <h3 v-for="index in schedules.length" :key="index">
       <span v-for="section in schedules[index]" :key="section.CourseNumber">
-        {{ section.Subject }}{{ section.CourseNumber }}: {{ section.Instructor }} ; {{ section.StartTime }} -
-        {{ section.EndTime }} ||
+        {{ section.Subject }}{{ section.CourseNumber }}: {{ section.Instructor }} ;
+        {{ convertTime(section.StartTime) }} - {{ convertTime(section.EndTime) }} ||
       </span>
     </h3>
   </div>
@@ -17,6 +17,22 @@ export default defineComponent({
   name: "Schedules",
   props: {
     schedules: Array as PropType<Array<Schedule>>,
+  },
+  methods: {
+    convertTime(str: String): String {
+      if (str == "TBA") return "TBA";
+      if (str.length != 8) return "ERROR";
+      let hours: number = parseInt(str.substring(0, 2));
+      let minutes: string = str.substring(3, 5);
+      let end: string;
+      if (hours > 12) {
+        hours -= 12;
+        end = "PM";
+      } else {
+        end = "AM";
+      }
+      return `${hours}:${minutes} ${end}`;
+    },
   },
 });
 </script>
