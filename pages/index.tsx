f1556b65518c8621744cee66919c801281242f8c
Luciano Lim <lmlim@cpp.edu>
2022-07-20 09:45:39 -0700

    updates home page looks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Container, Stack, Typography, useMediaQuery } from "@mui/material";
+import { Box, Button, Container, Stack, SxProps, Typography, useMediaQuery } from "@mui/material";
 import Link from "next/link";
 import { ReactElement } from "react";
 
@@ -7,11 +7,15 @@ export default function Home() {
   return (
     <Container maxWidth='xl'>
       <Box id='homePage' sx={{ mt: 30 }}>
-        <Typography variant={size ? "h1" : "h2"}>CPP Scheduler</Typography>
+        <Typography variant={"h1"} sx={{ textAlign: { xs: "center", md: "left" } }}>
+          CPP Scheduler
+        </Typography>
         <Box id='subTitle'>
-          <Typography variant={size ? "h3" : "h5"}>Planning Resources for CalPoly Pomona students</Typography>
+          <Typography variant={"h2"} sx={{ textAlign: { xs: "center", md: "left" } }}>
+            Data & Resources for CalPoly Pomona Students
+          </Typography>
         </Box>
-        <Stack direction='row' sx={{ mt: 3 }} spacing={2}>
+        <Stack direction={{ xs: "column", md: "row" }} sx={{ mt: 3 }} spacing={2}>
           <NavButton link='/schedule-builder'>
             <Typography>Build a Schedule</Typography>
           </NavButton>
@@ -22,24 +26,26 @@ export default function Home() {
             <Typography>Course Data</Typography>
           </NavButton>
         </Stack>
-        <Box id='disclaimer' sx={{ position: "absolute", top: "93vh", left: "10vw", width: "80vw" }}>
+        {/* <Box id='disclaimer' sx={{ position: "absolute", top: "93vh", left: "10vw", width: "80vw" }}>
           <Typography variant='caption'>
             CPP Scheduler is not created by, managed by, endorsed by, or affiliated with Cal Poly Pomona or the
             California State University system. CPP Scheduler is a 100% student run open source project. We do not
             warrant the information provided on this website for accuracy or relevancy. For clarification on the
             information provided, please utilize official CPP resources and datasources
           </Typography>
-        </Box>
+        </Box> */}
       </Box>
     </Container>
   );
 }
 
-function NavButton(props: { link: string; children: ReactElement }) {
+function NavButton(props: { link: string; children: ReactElement; sx?: SxProps }) {
   return (
     <Link href={props.link}>
       <a style={{ textDecoration: "none" }}>
-        <Button variant='outlined'>{props.children}</Button>
+        <Button variant='outlined' sx={{ width: { xs: "100%" } }}>
+          {props.children}
+        </Button>
       </a>
     </Link>
   );
